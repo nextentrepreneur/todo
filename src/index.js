77c2edcfd9c86f2399e684063adb4fecd50faaf4
nextentrepreneur <roy.sujit6@gmail.com>
2018-12-10 22:59:40 +0530

    add index.css in styles dir, redux and react-redux installed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
-import './index.css';
+import '../src/styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
